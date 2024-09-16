51c257630f3b23cedff4a0af70e885cd09d9df57
kr0n1kal <77345149+kr0n1kal@users.noreply.github.com>
2024-09-15 18:28:41 -0600

    Update usuarios.ts
    Se añade la propia documentación del código para lograr entenderlo mejor

--- usuarios.ts
+++ usuarios.ts
@@ -1,2 +1,4 @@
+//EXPORTAMOS DOS ARRAYS QUE CONTENGAN LOS USUARIOS Y CONTRASEÑAS
+//PARA EL INICIO DE SESION EN EL MAIN
 export const usuarios = ["jose", "andres"];
 export const contraseñas = ["lopez", "diaz"];
